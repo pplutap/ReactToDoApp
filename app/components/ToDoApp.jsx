685daf9c19c494d1c528dc5f164cef8d91433ea8
Pawel Pluta <pawel.pluta@eo.pl>
2017-07-22 16:02:43 +0200

    Simple list rendering

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+    getInitialState: function () {
+      return {
+          todos: [
+              {
+                  id: 1,
+                  text: 'Walk the dog'
+              },
+              {
+                  id: 2,
+                  text: 'Clean the yard'
+              },
+              {
+                  id: 3,
+                  text: 'Wash carpet'
+              },
+              {
+                  id: 4,
+                  text: 'Clean windows'
+              }
+          ]
+      }
+    },
     render: function () {
+        var {todos} = this.state;
+
         return (
             <div>
-                ToDoApp.jsx
+                <ToDoList todos={todos}/>
             </div>
         )
     }
