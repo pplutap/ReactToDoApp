385287a65b4d022c89544b258e9eee9c8edb4d7c
Pawel Pluta <pawel.pluta@eo.pl>
2017-07-22 22:09:49 +0200

    Complete handleAddToDo

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ToDoList = require('ToDoList');
 var AddToDo = require('AddToDo');
 var ToDoSearch = require('ToDoSearch');
+var uuid = require('node-uuid');
 
 var ToDoApp = React.createClass({
     getInitialState: function () {
@@ -10,26 +11,35 @@ var ToDoApp = React.createClass({
           searchText: '',
           todos: [
               {
-                  id: 1,
+                  id: uuid(),
                   text: 'Walk the dog'
               },
               {
-                  id: 2,
+                  id: uuid(),
                   text: 'Clean the yard'
               },
               {
-                  id: 3,
+                  id: uuid(),
                   text: 'Wash carpet'
               },
               {
-                  id: 4,
+                  id: uuid(),
                   text: 'Clean windows'
               }
           ]
       }
     },
     handleAddTodo: function (text) {
-      alert('new todo: ' + text);
+      this.setState({
+          todos: [
+              ...this.state.todos,
+              {
+                  id: uuid(),
+                  text: text
+              }
+
+          ]
+      });
     },
     handleSearch: function (showCompleted, searchText) {
       this.setState({
--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -9,5 +9,14 @@ var ToDoApp = require('ToDoApp');
 describe('ToDoApp', () => {
     it('should exists', () => {
         expect(ToDoApp).toExist();
-    })
+    });
+
+    it('should add todo to the todos state on handleTodo', () => {
+       var todoText = 'testowy tekst';
+       var toDoApp = TextUtils.renderIntoDocument(<ToDoApp/>);
+       toDoApp.setState({todos: []});
+       toDoApp.handleAddTodo(todoText);
+
+       expect(toDoApp.state.todos[0].text).toBe(todoText);
+    });
 });
