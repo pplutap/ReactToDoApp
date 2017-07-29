75eee69294b56d11338c8f58a154116e21888068
Pawel Pluta <pawel.pluta@eo.pl>
2017-07-29 11:35:17 +0200

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,16 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var ToDoApp = require('ToDoApp');
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the year'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 $(document).foundation();
 
